5b217ce9c0906dc66e09f25022e4188aa145f744
joshuakitong <joshuakitong@gmail.com>
2025-07-16 10:55:05 +0800

    refactor: fix code consistency

--- src/app/coinguessr/utils/fetchCoinGuessrCoins.js
+++ src/app/coinguessr/utils/fetchCoinGuessrCoins.js
@@ -1,5 +1,5 @@
 export async function fetchCoinGuessrCoins() {
-  const res = await fetch('/api/coins');
+  const res = await fetch("/api/coins");
   if (!res.ok) throw new Error("Failed to fetch coins");
 
   return await res.json();
