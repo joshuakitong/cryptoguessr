21f3aa8d3447766327e8f29f62921d37ef8b7046
joshuakitong <joshuakitong@gmail.com>
2025-07-18 07:03:27 +0800

    fix: updated fetch coins for coinguessr to return top 2000 instead of 1000

--- src/app/api/coins/route.js
+++ src/app/api/coins/route.js
@@ -1,7 +1,7 @@
 export async function GET() {
   const res = await fetch(
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd' +
-    '&order=market_cap_desc&per_page=1000&page=1&sparkline=false'
+    '&order=market_cap_desc&per_page=2000&page=1&sparkline=false'
   );
   const data = await res.json();
 
