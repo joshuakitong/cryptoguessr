d3053acf1ad54129f7f99b93266ee42572b33a22
joshuakitong <joshuakitong@gmail.com>
2025-07-18 07:34:42 +0800

    fix: adjust gain over labels

--- src/app/gainover/components/CryptoCompareBox.jsx
+++ src/app/gainover/components/CryptoCompareBox.jsx
@@ -45,7 +45,8 @@ export default function CryptoCompareBox({ coin, onClick, revealed, metric, isCo
         {revealed && typeof value === "number" ? (
           <AnimatedNumber
             value={value}
-            suffix={metric.includes("price_change") ? "%" : "$"}
+            prefix={metric.includes("current_price") ?? "$"}
+            suffix={metric.includes("price_change") ?? "%"}
             dynamicDecimals
           />
         ) : ""}
