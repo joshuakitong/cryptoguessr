dd44ef64e334731711754c3ae3a57877d4bd89e9
joshuakitong <joshuakitong@gmail.com>
2025-07-18 21:05:42 +0800

    docs: update coinguessr game info

--- src/app/data/gameInfo.js
+++ src/app/data/gameInfo.js
@@ -1,15 +1,15 @@
 export const coinGuessrGameInfo = `
-Guess the name of 10 random cryptocurrencies, one letter at a time (just like Hangman).
+Guess the name of random cryptocurrencies, one letter at a time (just like Hangman).
 
 Mechanics:
 - Start with 10 lives.
 - Each correct letter reveals part of the name.
 - Guess incorrectly and lose 1 life.
 - Guess the entire name to earn +100 points and +3 extra lives (max 10).
-- Game ends when you lose all lives or complete all 10 coins.
+- Game ends when you lose all lives or get 10 correct guesses.
 
 Bonus:
-- Complete all 10 coins to earn a bonus: +100 points for each remaining life.
+- Get 10 correct guesses to earn a bonus: +100 points for each remaining life.
 `;
 
 export const bitdleGameInfo = `
