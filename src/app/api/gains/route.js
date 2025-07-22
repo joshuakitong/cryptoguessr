8e4af31395e41a13706fced5f2c97306277cc2ed
joshuakitong <joshuakitong@gmail.com>
2025-07-22 08:42:56 +0800

    feat: update api calls to return top 250 coins

--- src/app/api/gains/route.js
+++ src/app/api/gains/route.js
@@ -1,8 +1,7 @@
 export async function GET() {
   const res = await fetch(
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd' +
-    '&order=market_cap_desc&sparkline=false' +
-    '&price_change_percentage=24h,7d,30d,1y'
+    '&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h,7d,30d,1y'
   );
   const allCoins = await res.json();
 
