72c5c0bc85af798eed36598601fd173b225f6bbd
joshuakitong <joshuakitong@gmail.com>
2025-07-22 08:06:06 +0800

    fix: adjust bitdle vote buttons ux

--- src/app/bitdle/components/VoteButtons.jsx
+++ src/app/bitdle/components/VoteButtons.jsx
@@ -7,11 +7,11 @@ export default function VoteButtons({ vote, canVote, handleVote }) {
       <button
         onClick={() => handleVote("down")}
         disabled={!canVote}
-        className={`w-24 h-24 hover:scale-110 flex items-center justify-center rounded-full transition ${
+        className={`w-24 h-24 flex items-center justify-center rounded-full transition ${
           vote === "down"
             ? "bg-red-700 scale-110 ring-4 ring-red-400"
-            : "bg-red-600 hover:bg-red-700"
-        } ${!canVote ? "opacity-70 cursor-not-allowed" : "cursor-pointer"}`}
+            : "bg-red-600"
+        } ${!canVote ? "opacity-70 cursor-not-allowed" : "hover:scale-110 hover:bg-red-700 cursor-pointer"}`}
         title="Vote Down"
       >
         <ArrowBigDown className="w-12 h-12 text-red-300 transition-all" />
@@ -20,11 +20,11 @@ export default function VoteButtons({ vote, canVote, handleVote }) {
       <button
         onClick={() => handleVote("up")}
         disabled={!canVote}
-        className={`w-24 h-24 hover:scale-110 flex items-center justify-center rounded-full transition ${
+        className={`w-24 h-24 flex items-center justify-center rounded-full transition ${
           vote === "up"
             ? "bg-green-700 scale-110 ring-4 ring-green-400"
-            : "bg-green-600 hover:bg-green-700"
-        } ${!canVote ? "opacity-70 cursor-not-allowed" : "cursor-pointer"}`}
+            : "bg-green-600"
+        } ${!canVote ? "opacity-70 cursor-not-allowed" : "hover:scale-110 hover:bg-green-700 cursor-pointer"}`}
         title="Vote Up"
       >
         <ArrowBigUp className="w-12 h-12 text-green-300 transition-all" />
